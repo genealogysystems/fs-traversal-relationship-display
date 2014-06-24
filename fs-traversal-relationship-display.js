89c66965ed812189ee3d9f9e1714a080cef7a7e1
Justin <justincyork@gmail.com>
2014-06-24 17:29:43 -0500

    Calculate relative height and left positions

--- fs-traversal-relationship-display.js
+++ fs-traversal-relationship-display.js
@@ -14,8 +14,41 @@
       });
     }
     
+    // Calculate height
+    var prevHeight = path[0].height = 0;
+    var prevChange = path[0].change = 0;
+    path[0].corner = false;
+    for(var i = 1; i < path.length; i++){
+      var change = 0;
+      switch(path[i].relationship){
+        case 'child':
+          change = -1;
+          break;
+        case 'mother':
+        case 'father':
+          change = 1;
+          break;
+      }
+      prevHeight = path[i].height = prevHeight + change;
+      path[i-1].corner = (change === 1 && prevChange === -1) || (prevChange === 1 && change === -1);
+      prevChange = path[i].change = change;
+    }
+    path[path.length-1].corner = false;
+    
+    // Calculate left
+    var prevLeft = path[0].left = 0;
+    for(var i = 1; i < path.length-1; i++){
+      var change = 0;
+      if(path[i-1].corner || path[i].corner){
+        change = .5;
+      }
+      else if(path[i].relationship === 'spouse'){
+        change = 1;
+      }
+      prevLeft = path[i].left = prevLeft + change;
+    }
+    
     console.log(path);
-  
   };
 
 }());
